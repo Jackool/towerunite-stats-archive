17e54b96848c85195904aa329319868c60832373
Jackool <contact@jackool.com>
2020-10-25 08:18:05 -0400

    Add formatted version of JSON stats data because why not

--- stats-update.sh
+++ stats-update.sh
@@ -1,4 +1,4 @@
 steamAPIKey=`cat steam-api-key.ini`
 (wget -S -d -v -O /dev/stdout "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=394690&key=$steamAPIKey&steamid=76561198096770137" 2>76561198096770137/stats.log 1>76561198096770137/stats.json)
 sed -i "s/$steamAPIKey/SNIPPED/g" 76561198096770137/stats.log
-
+jq '.' 76561198096770137/stats.json > 76561198096770137/stats.formatted.json
