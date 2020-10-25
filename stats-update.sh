4131263a150f110cd3de87805ad685a0e777d74e
Jackool <contact@jackool.com>
2020-10-25 08:34:45 -0400

    Automatically update git repo with timestamps when stats-update.sh runs 2020-10-25T12:34:45Z

--- stats-update.sh
+++ stats-update.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+cd "${0%/*}"
+
 steamAPIKey=`cat steam-api-key.ini`
+
+timeISO=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
 (wget -S -d -v -O /dev/stdout "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=394690&key=$steamAPIKey&steamid=76561198096770137" 2>76561198096770137/stats.log 1>76561198096770137/stats.json)
+
 sed -i "s/$steamAPIKey/SNIPPED/g" 76561198096770137/stats.log
+
 jq '.' 76561198096770137/stats.json > 76561198096770137/stats.formatted.json
+
+(git add -A && git commit -m "Automatically update git repo with timestamps when stats-update.sh runs $timeISO" && git push -u origin main) || true
