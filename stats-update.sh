04ac6033aa675e4bce4a6d02403f381ee1728956
Jackool <contact@jackool.com>
2020-10-25 08:38:07 -0400

    Use SSL when requesting data from the Steam API

--- stats-update.sh
+++ stats-update.sh
@@ -6,7 +6,7 @@ steamAPIKey=`cat steam-api-key.ini`
 
 timeISO=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-(wget -S -d -v -O /dev/stdout "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=394690&key=$steamAPIKey&steamid=76561198096770137" 2>76561198096770137/stats.log 1>76561198096770137/stats.json)
+(wget -S -d -v -O /dev/stdout "https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=394690&key=$steamAPIKey&steamid=76561198096770137" 2>76561198096770137/stats.log 1>76561198096770137/stats.json)
 
 sed -i "s/$steamAPIKey/SNIPPED/g" 76561198096770137/stats.log
 
