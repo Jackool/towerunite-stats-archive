a1e920c4d7e8ff3e78422d271c273a6c22df8941
Jackool <contact@jackool.com>
2020-10-25 08:47:54 -0400

    2020-10-25T12:47:51Z

--- stats-update.sh
+++ stats-update.sh
@@ -2,14 +2,17 @@
 
 cd "${0%/*}"
 
+steamIDs=("76561198096770137" "76561198056506073")
+
 steamAPIKey=`cat steam-api-key.ini`
 
 timeISO=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-(wget -S -d -v -O /dev/stdout "https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=394690&key=$steamAPIKey&steamid=76561198096770137" 2>76561198096770137/stats.log 1>76561198096770137/stats.json)
-
-sed -i "s/$steamAPIKey/SNIPPED/g" 76561198096770137/stats.log
-
-jq '.' 76561198096770137/stats.json > 76561198096770137/stats.formatted.json
+for steamid64 in ${steamIDs[*]}; do
+   mkdir -p "$steamid64"
+   (wget -S -d -v -O /dev/stdout "https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=394690&key=$steamAPIKey&steamid=$steamid64" 2>"$steamid64/stats.log" 1>"$steamid64/stats.json")
+   sed -i "s/$steamAPIKey/SNIPPED/g" "$steamid64/stats.log"
+   jq '.' "$steamid64/stats.json" > "$steamid64/stats.formatted.json"
+done
 
 (git add -A && git commit -m "$timeISO" && git push -u origin main) || true
